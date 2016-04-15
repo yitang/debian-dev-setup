f1b4008c8aef4c698a9ef45cb08ad14e7c750d75
Yi Tang <yitang@col-67rwl12.jbanorthwest.co.uk>
2016-04-15 09:41:36 +0100

    system - add nlpot

---
+++ system.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Removed user's cached credentials
+# This script might be run with .dots, which uses elevated privileges
+sudo -K
+
+sudo apt-get install build-essential
+
+sudo apt-get install git
+
+sudo apt-get install libcurl4-openssl-dev 
+
+sudo apt-get install \
+     libnlopt \ # numerical optimiser 
+     libssl-dev
