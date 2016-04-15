b4e35fd8f40cc298e2f61b1fa6593af2e14f3412
Yi Tang <yitang@col-67rwl12.jbanorthwest.co.uk>
2016-04-15 09:39:31 +0100

    jekyll - add Pygments

--- others.sh
+++ others.sh
@@ -4,6 +4,7 @@ sudo -K
 
 sudo apt-get install ledger
 sudo apt-get install jekyll
+sudo apt-get pip3 install Pygments
 gem install s3_website
 sudo apt-get install gnome-shell
 apt-get install -y graphviz
