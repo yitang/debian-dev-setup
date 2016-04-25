4557ad30d4af740b7c53a55bf6d24edd1b6218ed
Yi Tang <yi.tang.uk@me.com>
2016-04-25 22:15:46 +0100

    format other.sh

--- others.sh
+++ others.sh
@@ -4,9 +4,9 @@ sudo -K
 
 sudo apt-get install ledger
 sudo apt-get install jekyll
-sudo apt-get pip3 install Pygments
+sudo pip3 install Pygments
 gem install s3_website
 sudo apt-get install gnome-shell
-apt-get install -y graphviz
+sudo apt-get install -y graphviz
 
 sudo pip3 install awscli
