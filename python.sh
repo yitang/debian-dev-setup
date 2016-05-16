4ac775877eb3ff931320bb7a73078de29c3e47c1
Yi Tang <yi.tang.uk@me.com>
2016-05-16 14:19:39 +0100

    python - add cython3

--- python.sh
+++ python.sh
@@ -23,7 +23,8 @@ sudo apt-get install \
      python3-scipy \
      python3-pandas \
      python3-seaborn \
-     python3-matplotlib 
+     python3-matplotlib \
+     cython3
      #     python3-scikit-learn  # only aviable on sid. 
 
 echo "Installing using pip"
