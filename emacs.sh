1e1f47ef4e24549f17fc4ccc61d207ab76a4afb2
Yi Tang <yi.tang.uk@me.com>
2016-08-17 16:37:30 +0100

    emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -12,7 +12,8 @@
 
 echo "------------------"
 echo "Installing required packages"
-sudo apt-get install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng12-dev librsvg2-dev libotf-dev libxml2-dev libcanberra-gtk-module
+sudo apt-get install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng12-dev librsvg2-dev libotf-dev libxml2-dev libcanberra-gtk-module libgnutls28-dev gnutls-bin curl
+
 
 echo "------------------"
 echo "Cloning emacs repo"
@@ -25,3 +26,8 @@ cd emacs
 ./configure
 make bootstrap
 sudo make install
+
+
+
+echo "need to install cask under user level"
+## curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
