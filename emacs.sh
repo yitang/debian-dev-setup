1484f1c9b0bc029867e82275881300888dc7ee1b
Yi Tang <6054101+yitang@users.noreply.github.com>
2024-09-03 20:07:25 +0100

    update

--- emacs.sh
+++ emacs.sh
@@ -3,13 +3,15 @@
 # for MacOS, use https://github.com/d12frosted/homebrew-emacs-plus
 sudo apt-get build-dep emacs
 sudo apt-get install libtree-sitter-dev
+sudo apt-get install libmagick++-dev  libmagickcore-dev
+sudo apt-get install libwebkit2gtk-4.1-dev
 
 # get emacs source code
 # git clone -b  emacs-29 --single-branch git://git.savannah.gnu.org/emacs.git emacs-29
 
 # download Emacs direclty instead of clone git repo - its a lot faster.
-wget http://mirrors.nav.ro/gnu/emacs/emacs-29.1.tar.xz
-tar xvf emacs-29.1.tar.xz
+wget http://mirrors.nav.ro/gnu/emacs/emacs-29.4.tar.xz
+tar xvf emacs-29.4.tar.xz
 
 cd emacs-29.1/
 ./autogen.sh
